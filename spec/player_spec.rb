1eded185f536c3e7cc5b531eaacb029dbd4e2cb3
danilozag1992 <danilozagarcanin@gmail.com>
2019-08-20 12:20:43 +0200

    corrected errors

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,31 +1,32 @@
 # frozen_string_literal: true
 
-require './lib/player'
+require "./lib/player"
 
 describe Player do
   let(:player) { Player.new(type) }
-  context 'Empty' do
-    let(:type) { {} }
-    it 'checks where there is no value' do
-      expect { player }.to raise_error
+  context "Empty" do
+    let(:type) { { choice: " ", name: " " } }
+
+    it "checks where there is no value" do
+      expect { raise Player }.to raise_error { |error| expect(error).to be_a(TypeError) }
     end
   end
-  context 'inputed values' do
-    let(:type) { { choice: 'X', name: 'Chubi' } }
-    it 'checks where there is no value' do
+  context "inputed values" do
+    let(:type) { { choice: "X", name: "Chubi" } }
+    it "checks where there is no value" do
       expect { player }.to_not raise_error
     end
   end
-  context 'inputed choice values' do
-    let(:type) { { choice: 'X', name: 'Chubi' } }
-    it 'checks where there is no value' do
-      expect(player.choice).to eql('X')
+  context "inputed choice values" do
+    let(:type) { { choice: "X", name: "Chubi" } }
+    it "checks where there is no value" do
+      expect(player.choice).to eql("X")
     end
   end
-  context 'inputed name values' do
-    let(:type) { { choice: 'X', name: 'Chubi' } }
-    it 'checks where there is no value' do
-      expect(player.name).to eql('Chubi')
+  context "inputed name values" do
+    let(:type) { { choice: "X", name: "Chubi" } }
+    it "checks where there is no value" do
+      expect(player.name).to eql("Chubi")
     end
   end
 end
